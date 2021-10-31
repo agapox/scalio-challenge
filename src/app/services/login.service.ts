8ff6223015182c3a016a97b68f1a7ec73e1c8d59
Jose Agapito Ramirez <agapox@gmail.com>
2021-10-31 16:02:12 -0300

    feature: create LoginSservice to call github API and retreive list of login

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+// import { Character, CharactersHttp } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private APIURL: string = 'https://api.github.com/search/users?q=';
+
+  constructor(
+    private httpClient: HttpClient,
+  ) {}
+
+
+  getLogin(login: string) {
+    return this.httpClient.get(`${this.APIURL}${login}`)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error)
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.status);
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body: ${error.error}`);
+    }
+    return throwError(
+      'Error: please try again later.');
+  }
+  
+}
