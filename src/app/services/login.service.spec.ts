3f079a3e9b95e0f7b41abf8d6c470fa49902b443
Jose Agapito Ramirez <agapox@gmail.com>
2021-10-31 16:32:33 -0300

    feature: adding test to getLogin method

--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -1,16 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { getTestBed, inject, TestBed } from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { loginMock } from './login.mock';
 
 import { LoginService } from './login.service';
 
-describe('LoginService', () => {
+fdescribe('LoginService', () => {
   let service: LoginService;
+  let httpMock: HttpTestingController;
+  let injector: TestBed;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    injector = getTestBed();
+    httpMock = injector.inject(HttpTestingController);
     service = TestBed.inject(LoginService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get list of users', () => {
+    const textOnInputMock = 'agapox';
+    expect(service).toBeTruthy();
+    service.getLogin(textOnInputMock).subscribe((data: any) => {
+      expect(data).toBeTruthy();
+      expect(data).toBe(usersResponse)
+      expect(data.total_count).toBe(2)
+      expect(data).toBe(usersResponse)
+      expect(data.items[0].login).toBe('agapox')
+    });
+
+    const usersResponse = loginMock;
+    const req = httpMock.expectOne({
+      method: "GET",
+      url: `https://api.github.com/search/users?q=${textOnInputMock}`
+    });
+  
+    req.flush(usersResponse);
+  
+    httpMock.verify();
+  })
+
+
+  it('should console error if failed', () => {
+    
+  });
+
 });
